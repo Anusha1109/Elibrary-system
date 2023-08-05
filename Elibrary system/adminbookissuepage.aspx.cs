e5210849c2f227a09613487418687bb3a347172c
Anusha1109 <119651241+Anusha1109@users.noreply.github.com>
2023-08-06 00:03:42 +0530

    Add project files.

---
+++ Elibrary system/adminbookissuepage.aspx.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Elibrary_system
+{
+    public partial class adminbookissuepage : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getnames();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (checkifbookexists() && checkifmemberexists())
+            {
+                if (checkifissueentryexists())
+                {
+                    Response.Write("<script>alert('this member already have this book');</script>");
+                }
+                else
+                {
+                    issuebooks();
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('wrong book ID or member id');</script>");
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (checkifbookexists() && checkifmemberexists())
+            {
+                if (checkifissueentryexists())
+                {
+
+                    returnbook();//Response.Write("<script>alert('this member already have this book');</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('this entry doesnot exists');</script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('wrong book ID or member id');</script>");
+            }
+        }
+        void returnbook()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("DELETE from book_issue_tbl WHERE book_id='" + TextBox1.Text.Trim() + "' AND member_id='" + TextBox2.Text.Trim() + "'", con);
+
+                int result = cmd.ExecuteNonQuery();
+               
+                if (result > 0)
+                {
+           cmd = new SqlCommand("update book_master_tbl set current_stock=current_stock+1 WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("<script>alert('book returned Successfully');</script>");
+                    
+                    GridView1.DataBind();
+                    con.Close();
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('invalid details');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+
+
+
+
+
+        //userdefiend codes
+        void issuebooks()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO book_issue_tbl(member_id,member_name,book_id,book_name,issue_date,due_date) values(@member_id,@member_name,@book_id,@book_name,@issue_date,@due_date)", con);
+
+                cmd.Parameters.AddWithValue("@member_id", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@member_name", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_name", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@issue_date", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@due_date", TextBox6.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand("UPDATE book_master_tbl set current_stock=current_stock-1 WHERE book_id='" + TextBox1.Text.Trim() + "'AND current_stock>0'", con);
+        
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Book issued Successfully');</script>");
+              
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        bool checkifbookexists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from book_master_tbl where book_id='" + TextBox1.Text.Trim() + "'AND current_stock>0", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //  Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+
+        bool checkifmemberexists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT full_name from member_master_tbl where member_id='" + TextBox2.Text.Trim() +"'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                //  Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+        bool checkifissueentryexists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from book_issue_tbl where member_id='" + TextBox2.Text.Trim() + "'AND book_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                //  Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+
+
+        void getnames()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT book_name from book_master_tbl where book_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox4.Text = dt.Rows[0]["book_name"].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('wrong book ID');</script>");
+                }
+                cmd = new SqlCommand("SELECT full_name from member_master_tbl where member_id='" + TextBox2.Text.Trim() + "'", con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox3.Text = dt.Rows[0]["full_name"].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('wrong user ID');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                //  Response.Write("<script>alert('" + ex.Message + "');</script>");
+                // return false;
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            try
+            {
+                if(e.Row.RowType==DataControlRowType.DataRow)
+                {
+                    DateTime dt = Convert.ToDateTime(e.Row.Cells[5].Text);
+                    DateTime today = DateTime.Today;
+                    if(today>dt)
+                    {
+                        e.Row.BackColor = System.Drawing.Color.PaleVioletRed;
+                    }
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+    } }
+
+
+
+
+
+
---
+++ Elibrary system/homepage.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="Elibrary_system.WebForm3" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+ <section>
+      <img src="imgs/home-bg.jpg" class="img-fluid"/>
+   </section>
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <center>
+                  <h2>Our Features</h2>
+                  <p><b>Our 3 Primary Features -</b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/digital-inventory.png"/>
+                  <h4>Digital Book Inventory</h4>
+                  <p class="text-justify">A digital book inventory typically contains a database or list of digital books available for access or purchase.</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/search-online.png"/>
+                  <h4>Search Books</h4>
+                  <p class="text-justify">If you're looking for a particular book page, I recommend using a search engine like Google and entering the book's title, author, or relevant keywords along with "book page".</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/defaulters-list.png"/>
+                  <h4>Defaulter List</h4>
+                  <p class="text-justify">The defaulter list is an essential tool for library staff to manage overdue items and ensure that all borrowed materials are returned promptly. It helps to track users who have outstanding items and take appropriate actions to recover the materials and resolve any fines or penalties.</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section>
+      <img src="imgs/in-homepage-banner.jpg" class="img-fluid"/>
+   </section>
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <center>
+                  <h2>Our Process</h2>
+                  <p><b>We have a Simple 3 Step Process</b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/sign-up.png" />
+                  <h4>Sign Up</h4>
+                  <p class="text-justify">A signup page is a web page that allows users to create new accounts or sign up for a service, website, or application. It's a crucial step in the user registration process. </p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/search-online.png"/>
+                  <h4>Search Books</h4>
+                  <p class="text-justify">If you are looking for book links or specific information about books, I recommend using online bookstores, library catalogs, or search engines to find the information you need. You can enter the title, author, or relevant keywords related to the books you are interested in, and the search engine will provide you with relevant results</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/library.png"/>
+                  <h4>Visit Us</h4>
+                  <p class="text-justify">If you have any specific questions or need assistance with anything else, feel free to ask, and I'll be happy to help to the best of my ability.</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+</asp:Content>
